79e41ec7d5ea630a750e38c49765b7fdd0bb1c00
Łukasz Barulski <tracerout@yahoo.com>
2018-03-21 15:47:26 +0100

    init

---
+++ request-debugger.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "sort"
+    "bytes"
+    "time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    var requestHeaders []string
+    for k := range r.Header {
+        requestHeaders = append(requestHeaders, k)
+    }
+    sort.Strings(requestHeaders)
+
+    writer := bytes.Buffer{}
+    fmt.Fprintln(&writer, "============ NEW REQUEST ============")
+
+    fmt.Fprintln(&writer, "request.RequestURI:", r.RequestURI)
+    fmt.Fprintln(&writer, "request.Method:", r.Method)
+    fmt.Fprintln(&writer, "request.RemoteAddr:", r.RemoteAddr)
+    fmt.Fprintln(&writer, "request.TLS:", r.TLS)
+
+    fmt.Fprintln(&writer, "Request Headers:")
+    for _, k := range requestHeaders {
+        fmt.Fprintln(&writer, "\t", k, ":", r.Header[k])
+    }
+
+    fmt.Println(writer.String())
+
+    w.Write([]byte("Request debugger (" + (time.Now()).Format(time.RFC3339) + ")"))
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":80", nil)
+}
